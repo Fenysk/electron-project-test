b727a13a387632d1814df9ac00e4cbda53680a75
Fenysk <minefenysk@gmail.com>
2023-06-09 21:53:50 +0200

    ajout du modal

--- todolist/src/views/StockView.vue
+++ todolist/src/views/StockView.vue
@@ -2,6 +2,7 @@
   <div id="stock-view" class="
   p-8 bg-gray-100
   h-screen w-full
+  overflow-y-auto
   ">
     <h2 class="text-center">Jeux-vidéo en stock</h2>
 
@@ -13,24 +14,28 @@
       }}</button>
     </header>
 
-    <div class="modal" v-if="openCreateModal">
-      <form @submit.prevent="postGame(newGameToStockForm)" class="flex flex-col items-center">
+    <div class="modal bg-gray-100 flex items-center" v-if="openCreateModal">
+      <form @submit.prevent="postGame(newGameToStockForm)"
+        class="game border-2 border-gray-400 rounded-2xl bg-gray-800 text-white px-12 py-8 w-fit mx-auto overflow-hidden flex flex-col gap-8">
+        <h2 class="text-3xl font-bold mb-8">Ajouter un jeu</h2>
+        <div class="informations flex flex-col gap-4">
 
-        <div class="flex flex-col gap-3 items-center">
-
-          <div class="flex items-center gap-4 w-full">
-            <label class="text-right basis-1/3">Titre</label>
-            <input type="text" v-model="newGameToStockForm.title" placeholder="Nom du jeu" />
+          <div class="flex gap-4 items-center w-full">
+            <label class="text-right w-1/3">Titre</label>
+            <input type="text" v-model="newGameToStockForm.title" placeholder="Nom du jeu"
+              class="rounded-md bg-gray-200 text-black px-2 py-1 w-2/3" />
           </div>
 
-          <div class="flex items-center gap-4 w-full">
-            <label class="text-right basis-1/3">Image</label>
-            <input type="text" v-model="newGameToStockForm.image" placeholder="URL de l'image" />
+          <div class="flex gap-4 items-center w-full">
+            <label class="text-right w-1/3">Image</label>
+            <input type="text" v-model="newGameToStockForm.image" placeholder="URL de l'image"
+              class="rounded-md bg-gray-200 text-black px-2 py-1 w-2/3" />
           </div>
 
-          <div class="flex items-center gap-4 w-full">
-            <label class="text-right basis-1/3">Plateforme</label>
-            <select v-model="newGameToStockForm.platform">
+          <div class="flex gap-4 items-center w-full">
+            <label class="text-right w-1/3">Plateforme</label>
+            <select v-model="newGameToStockForm.platform"
+            class="rounded-md bg-gray-200 text-black px-2 py-1 w-2/3">
               <option value="Nintendo Switch">Nintendo Switch</option>
               <option value="PlayStation 4">PlayStation 4</option>
               <option value="PlayStation 5">PlayStation 5</option>
@@ -40,52 +45,65 @@
             </select>
           </div>
 
-          <div class="flex items-center gap-4 w-full">
-            <label class="text-right basis-1/3">Edition</label>
-            <input type="text" v-model="newGameToStockForm.edition" placeholder="Édition" />
+          <div class="flex gap-4 items-center w-full">
+            <label class="text-right w-1/3">Edition</label>
+            <input type="text" v-model="newGameToStockForm.edition" placeholder="Édition"
+              class="rounded-md bg-gray-200 text-black px-2 py-1 w-2/3" />
           </div>
 
-          <div class="flex items-center gap-4 w-full">
-            <label class="text-right basis-1/3">État</label>
-            <input type="text" v-model="newGameToStockForm.state" placeholder="État" />
+          <div class="flex gap-4 items-center w-full">
+            <label class="text-right w-1/3">État</label>
+            <select v-model="newGameToStockForm.state" class="rounded-md bg-gray-200 text-black px-2 py-1 w-2/3">
+              <option value="Neuf">Neuf</option>
+              <option value="Très bon état">Très bon état</option>
+              <option value="Bon état">Bon état</option>
+              <option value="Moyen">Moyen</option>
+              <option value="Mauvais">Mauvais</option>
+              <option value="Mauvais">Pour pièces</option>
+            </select>
           </div>
 
           <div class="w-full">
-            <div class="flex items-center gap-4 w-full">
-              <label class="text-right basis-1/3">Contenu</label>
-              <input type="text" v-model="newGameToStockForm.content" placeholder="Contenu" />
-              <button type="button" @click="addContent">Ajouter</button>
+            <div class="flex gap-4 items-center w-full">
+              <label class="text-right w-1/3">Contenu</label>
+              <input type="text" v-model="newGameToStockForm.content" placeholder="Contenu"
+                class="rounded-md bg-gray-200 text-black px-2 py-1 w-1/3" />
+              <button type="button" @click="addContent"
+                class="bg-green-100 text-gray-800 border-none px-2 py-1 rounded-md text-sm font-bold transition duration-200 hover:bg-green-300">Ajouter</button>
             </div>
-            <ul class="flex justify-center gap-1">
-              <li v-for="content in newGameToStockForm.contents"
-              :key="content"
-              class="px-4 py-2 border-2 border-gray-400 rounded-md mt-2 relative cursor-pointer"
-              @click="removeContent(content)"
-              >
+            <ul class="flex justify-center gap-1 mt-2">
+              <li v-for="content in newGameToStockForm.contents" :key="content"
+                class="px-4 py-2 rounded-md relative cursor-pointer text-sm bg-gray-200 text-black hover:scale-90 transition duration-200"
+                @click="removeContent(content)">
                 {{ content }}
               </li>
             </ul>
           </div>
 
-          <div class="flex items-center gap-4 w-full">
-            <label class="text-right basis-1/3">Prix d'achat</label>
-            <input type="number" v-model="newGameToStockForm.buyPrice" placeholder="Prix d'achat" />
+          <div class="flex gap-4 items-center w-full">
+            <label class="text-right w-1/3">Prix d'achat</label>
+            <input type="number" v-model="newGameToStockForm.buyPrice" placeholder="Prix d'achat"
+              class="rounded-md bg-gray-200 text-black px-2 py-1 w-2/3" />
           </div>
 
-          <div class="flex items-center gap-4 w-full">
-            <label class="text-right basis-1/3">Prix estimé</label>
-            <input type="number" v-model="newGameToStockForm.potentialSellPrice" placeholder="Prix de vente potentiel" />
+          <div class="flex gap-4 items-center w-full">
+            <label class="text-right w-1/3">Prix estimé</label>
+            <input type="number" v-model="newGameToStockForm.potentialSellPrice" placeholder="Prix de vente potentiel"
+              class="rounded-md bg-gray-200 text-black px-2 py-1 w-2/3" />
           </div>
 
-          <div class="flex items-center gap-4 w-full">
-            <label class="text-right basis-1/3">Informations complémentaires</label>
-            <input type="text" v-model="newGameToStockForm.additonalInfos" placeholder="Informations supplémentaires" />
+          <div class="flex gap-4 items-center w-full">
+            <label class="text-right w-1/3">Informations complémentaires</label>
+            <input type="text" v-model="newGameToStockForm.additonalInfos" placeholder="Informations supplémentaires"
+              class="rounded-md bg-gray-200 text-black px-2 py-1 w-2/3" />
           </div>
         </div>
 
-        <button type="submit">Ajouter</button>
+        <button type="submit"
+          class="bg-green-100 text-gray-800 border-none px-8 py-2 rounded-md text-xl font-bold transition duration-200 hover:bg-green-300 mt-8">Ajouter au stock</button>
       </form>
     </div>
+
 
     <ul v-if="!openCreateModal" class="games-list
     flex flex-wrap gap-4
@@ -114,10 +132,10 @@ export default {
       games: [],
       newGameToStockForm: {
         title: 'Super Smash Bros. Ultimate',
-        image: 'https://mario.wiki.gallery/images/thumb/f/fc/SSB_Ultimate_box_art.png/400px-SSB_Ultimate_box_art.png',
-        platform: 'Nintendo Switch',
+        image: 'https://gameinferno.fr/wp-content/uploads/2013/12/Super_Mario_Bros._NA-438x600.png',
+        platform: 'Autre',
         edition: 'Standard',
-        state: 'Neuf',
+        state: 'Bon état',
         contents: ['Jeu', 'Boîte', 'Notice'],
         buyPrice: 20,
         potentialSellPrice: 40,
@@ -134,7 +152,7 @@ export default {
       this.newGameToStockForm.contents.push(this.newGameToStockForm.content)
       this.newGameToStockForm.content = ''
     },
-    
+
     removeContent(content) {
       this.newGameToStockForm.contents = this.newGameToStockForm.contents.filter(c => c !== content)
     },
